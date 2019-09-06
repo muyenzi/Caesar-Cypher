33198e1e6d11a519bf97d3110a72cede6d5a2418
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-06 21:12:51 +0200

    created the encryption method

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,4 +1,16 @@
 public class Encryption {
+    public String runCypher(String words , int key){
+
+        int length=words.length();
+        int choice = 0;
+        char word;
+        if(choice == 1) {
+            for (int i=0;i<length;i++){
+             word=i
+
+        }
+
+    }
 
 
 }
