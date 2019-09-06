aaadefd736153477297ddd055f37fe163d6a0d6e
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-06 22:46:14 +0200

    created character to hold the string characters

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -5,12 +5,41 @@ public class Encryption {
         int choice = 0;
         char word;
         if(choice == 1) {
-            for (int i=0;i<length;i++){
-             word=i
+            for (int i = 0; i <length; i++) {
+                word=words.charAt(i);
+                if(word >= 'a' && word <= 'z'){
+                    word=(char)(word + key) ;
+                    if(word)
+                }
+            }
 
+            }
+            }
         }
-
     }
 
 
-}
+//    String ciphertext = "";
+//    char alphabet;
+//        for(int i=0; i < plaintext.length();i++)
+//        {
+//        // Shift one character at a time
+//        alphabet = plaintext.charAt(i);
+//
+//        // if alphabet lies between a and z
+//        if(alphabet >= 'a' && alphabet <= 'z')
+//        {
+//        // shift alphabet
+//        alphabet = (char) (alphabet + shift);
+//        // if shift alphabet greater than 'z'
+//        if(alphabet > 'z') {
+//        // reshift to starting position
+//        alphabet = (char) (alphabet+'a'-'z'-1);
+//        }
+//        ciphertext = ciphertext + alphabet;
+//        }
+//
+//        // if alphabet lies between 'A'and 'Z'
+//        else if(alphabet >= 'A' && alphabet <= 'Z') {
+//        // shift alphabet
+//        alphabet = (char) (alphabet + shift);
