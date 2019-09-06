d4ec23baf46305a32178011468ac217844d14f03
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-07 01:42:02 +0200

    created the app class

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.Console;
+
+public class App {
+
+    public class App {
+        public static void main(String[] args) {
+            Console myConsole = System.console();
+            System.out.println("Encryption");
+            System.out.println("Enter the message :");
+            String Words = myConsole.readLine();
+
+
+
+        }
+    }
+}
